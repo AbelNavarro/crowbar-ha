e8a497f26ef69e8ef7d03285a3b92e5666beb03d
Abel Navarro <abel.navarro@gmail.com>
2017-11-06 17:25:22 +0100

    added missing operations for primitive

--- chef/cookbooks/crowbar-pacemaker/providers/primitive.rb
+++ chef/cookbooks/crowbar-pacemaker/providers/primitive.rb
@@ -18,3 +18,32 @@ action :update do
   end
 end
 
+action :delete do
+  pacemaker_primitive :name do
+    agent :agent
+    params :params
+    meta :meta
+    op :op
+    action :delete
+  end
+end
+
+action :start do
+  pacemaker_primitive :name do
+    agent :agent
+    params :params
+    meta :meta
+    op :op
+    action :start
+  end
+end
+
+action :stop do
+  pacemaker_primitive :name do
+    agent :agent
+    params :params
+    meta :meta
+    op :op
+    action :stop
+  end
+end
