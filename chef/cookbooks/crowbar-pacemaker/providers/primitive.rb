e46e7428a4f6002c237910f764ccfc85989b5451
Abel Navarro <abel.navarro@gmail.com>
2017-11-06 16:43:19 +0100

    added :update to provider

--- chef/cookbooks/crowbar-pacemaker/providers/primitive.rb
+++ chef/cookbooks/crowbar-pacemaker/providers/primitive.rb
@@ -8,3 +8,13 @@ action :create do
   end
 end
 
+action :update do
+  pacemaker_primitive :name do
+    agent :agent
+    params :params
+    meta :meta
+    op :op
+    action :update
+  end
+end
+
