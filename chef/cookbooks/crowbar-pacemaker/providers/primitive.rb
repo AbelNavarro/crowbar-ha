65e239e57b13843b3ecf7ec42f4abe0258546e68
Abel Navarro <abel.navarro@gmail.com>
2017-11-07 16:24:11 +0100

    more new_resource substitutions

--- chef/cookbooks/crowbar-pacemaker/providers/primitive.rb
+++ chef/cookbooks/crowbar-pacemaker/providers/primitive.rb
@@ -1,10 +1,10 @@
 action :create do
   Chef::Log.warn("XXX :create")
   pacemaker_primitive new_resource.name do
-    agent :agent
-    params :params
-    meta :meta
-    op :op
+    agent new_resource.agent
+    params new_resource.params
+    meta new_resource.meta
+    op new_resource.op
     action :create
   end
 end
@@ -12,40 +12,40 @@ end
 action :update do
   Chef::Log.warn("XXX :update")
   pacemaker_primitive new_resource.name do
-    agent :agent
-    params :params
-    meta :meta
-    op :op
+    agent new_resource.agent
+    params new_resource.params
+    meta new_resource.meta
+    op new_resource.op
     action :update
   end
 end
 
 action :delete do
   pacemaker_primitive new_resource.name do
-    agent :agent
-    params :params
-    meta :meta
-    op :op
+    agent new_resource.agent
+    params new_resource.params
+    meta new_resource.meta
+    op new_resource.op
     action :delete
   end
 end
 
 action :start do
   pacemaker_primitive new_resource.name do
-    agent :agent
-    params :params
-    meta :meta
-    op :op
+    agent new_resource.agent
+    params new_resource.params
+    meta new_resource.meta
+    op new_resource.op
     action :start
   end
 end
 
 action :stop do
   pacemaker_primitive new_resource.name do
-    agent :agent
-    params :params
-    meta :meta
-    op :op
+    agent new_resource.agent
+    params new_resource.params
+    meta new_resource.meta
+    op new_resource.op
     action :stop
   end
 end
