f95991583062ed6aa789a67d1cab137ac17766d0
Abel Navarro <abel.navarro@gmail.com>
2017-11-09 18:21:46 +0100

    op_defaults-logs16

--- chef/cookbooks/pacemaker/providers/primitive.rb
+++ chef/cookbooks/pacemaker/providers/primitive.rb
@@ -81,8 +81,8 @@ def update_resource(name)
   ops = new_resource.op
   ops.each { |option| Chef::Log.warn("XXX option: #{option}") }
   Chef::Log.warn("XXX ops.has_key?(monitor): #{ops.has_key?("monitor")}")
-  Chef::Log.warn("XXX ops monitor has key") unless !ops.has_key?("monitor")
-  Chef::Log.warn("XXX !ops['monitor'].has_key('on-fail')") unless !ops["monitor"].has_key?("on-fail")
+  #Chef::Log.warn("XXX ops monitor has key") unless !ops.has_key?("monitor")
+  #Chef::Log.warn("XXX !ops['monitor'].has_key('on-fail')") unless !ops["monitor"].has_key?("on-fail")
 
   #Chef::Log.warn("XXX node[:pacemaker][:config][:op_defaults]: #{node[:pacemaker][:config][:op_defaults]}")
   op_defaults = CrowbarPacemakerHelper.op_defaults(node)
