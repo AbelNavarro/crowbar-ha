4c1e761ad59e7663b9f7e68786a594c50defed4f
Abel Navarro <abel.navarro@gmail.com>
2017-11-10 10:29:08 +0100

    op_defaults-logs17

--- chef/cookbooks/pacemaker/providers/primitive.rb
+++ chef/cookbooks/pacemaker/providers/primitive.rb
@@ -94,6 +94,8 @@ def update_resource(name)
   #Chef::Log.warn("XXX node[:pacemaker][:config][:op_defaults]: #{node[:pacemaker][:config][:op_defaults]}")
   op_defaults = CrowbarPacemakerHelper.op_defaults(node)
   #op_d = node[:pacemaker][:config][:op_defaults]
+  #
+  Chef::Log.warn("XXX monitor.class(2): #{monitor.class}")
 
   ops.each { |option| Chef::Log.warn("XXX option3: #{option}") }
   
