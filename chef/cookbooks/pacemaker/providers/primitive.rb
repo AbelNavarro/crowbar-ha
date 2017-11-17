c4bd3914fc1192aeccdad3fa4f9ccfb09c7ee3b3
Abel Navarro <abel.navarro@gmail.com>
2017-11-17 15:43:58 +0100

    inotify

--- chef/cookbooks/pacemaker/providers/primitive.rb
+++ chef/cookbooks/pacemaker/providers/primitive.rb
@@ -92,7 +92,7 @@ def update_resource(name)
   #Chef::Log.warn("XXX !ops['monitor'].has_key('on-fail')") unless !ops["monitor"].has_key?("on-fail")
 
   #Chef::Log.warn("XXX node[:pacemaker][:config][:op_defaults]: #{node[:pacemaker][:config][:op_defaults]}")
-  #op_defaults = CrowbarPacemakerHelper.op_defaults(node)
+  op_defaults = CrowbarPacemakerHelper.op_defaults(node)
   #op_d = node[:pacemaker][:config][:op_defaults]
 
   #Chef::Log.warn("XXX ops.inspect(2): #{ops.inspect}")
