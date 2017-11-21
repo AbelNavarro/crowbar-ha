8d4ae13d50d20c63a3fae7a6d41340492bb9760d
Abel Navarro <abel.navarro@gmail.com>
2017-11-21 17:46:51 +0100

    inotify

--- chef/cookbooks/pacemaker/providers/primitive.rb
+++ chef/cookbooks/pacemaker/providers/primitive.rb
@@ -121,16 +121,16 @@ def update_resource(name)
     #ops["monitor"] = ops["monitor"].merge("on-fail" => op_defaults["monitor"]["on-fail"])
     #monitor.default = monitor.current_default.merge("on-fail" => op_defaults["monitor"]["on-fail"])
     #ops["monitor"] = { "on-fail" => "pepe" } 
-    if ops["monitor"].has_key?("on-fail")
-      Chef::Log.warn("XXX ops['monitor'].has_key('on-fail')")
+    #if ops["monitor"].has_key?("on-fail")
+    #  Chef::Log.warn("XXX ops['monitor'].has_key('on-fail')")
     #  ops = op_defaults["monitor"]["on-fail"]
-    else
-      Chef::Log.warn("XXX ops['monitor'].has_key('on-fail')... NOT")
+    #else
+      #Chef::Log.warn("XXX ops['monitor'].has_key('on-fail')... NOT")
       #ops.set_unless_value_present = true
       #Chef::Log.warn("XXX ops set_unless_value_present, inspect: #{ops.inspect}")
-      ops["monitor"]["on-fail"] = "block" # op_defaults["monitor"]["on-fail"]
+      ops["monitor"]["on-fail"] = op_defaults["monitor"]["on-fail"]
       #ops.set_unless_value_present = false
-    end
+    #end
     #ops.set["monitor"] = {"on-fail" => "bartolo"}
   end
 
