498767f45b89a9415235cc454bef0e61f9c193c5
Abel Navarro <abel.navarro@gmail.com>
2017-11-23 17:39:32 +0100

    inotify

--- chef/cookbooks/pacemaker/libraries/pacemaker/resource/primitive.rb
+++ chef/cookbooks/pacemaker/libraries/pacemaker/resource/primitive.rb
@@ -60,8 +60,8 @@ class Pacemaker::Resource::Primitive < Pacemaker::Resource
     %w(params meta op).each do |data_type|
       unless send(data_type).empty?
         data_string = send("#{data_type}_string")
-        Chef::Log.warn("XXX definition_from_attributes: #{data_string}")
         str << continuation_line(data_string)
+        Chef::Log.warn("XXX definition_from_attributes: #{data_string}")
       end
     end
     str
