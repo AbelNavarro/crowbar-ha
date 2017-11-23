093e215576e8d0cc03452e5ee0f6a9ed03230043
Abel Navarro <abel.navarro@gmail.com>
2017-11-23 15:03:09 +0100

    inotify

--- chef/cookbooks/pacemaker/providers/primitive.rb
+++ chef/cookbooks/pacemaker/providers/primitive.rb
@@ -114,7 +114,7 @@ def update_resource(name)
   if ops.has_key?("monitor")
     monitor_hash = ops.fetch("monitor")
     Chef::Log.warn("XXX monitor_hash class: #{monitor_hash.class}")
-    if monitor_hash.has_key("on-fail")
+    if monitor_hash.has_key?("on-fail")
       Chef::Log.warn("XXX has on-fail")
     else
       Chef::Log.warn("XXX has NOT on-fail")
