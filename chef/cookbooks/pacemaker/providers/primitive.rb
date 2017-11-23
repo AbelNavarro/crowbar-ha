dd1d2f5e44b40bad2f3da4303c31221685c83867
Abel Navarro <abel.navarro@gmail.com>
2017-11-23 18:09:10 +0100

    inotify

--- chef/cookbooks/pacemaker/providers/primitive.rb
+++ chef/cookbooks/pacemaker/providers/primitive.rb
@@ -77,9 +77,7 @@ end
 def update_resource(name)
   ops = new_resource.op
   Chef::Log.warn("XXX ops.inspect: #{ops.inspect}")
-  #Chef::Log.warn("XXX self.op.inspect: #{self.op.inspect}")
-  Chef::Log.warn("XXX self.params.inspect: #{self.params.inspect}")
-  #monitor = ops["monitor"]
+  Chef::Log.warn("XXX ops.inspect: #{@current_resource.ops.inspect}")
 
   #Chef::Log.warn("XXX node[:pacemaker][:config][:op_defaults]: #{node[:pacemaker][:config][:op_defaults]}")
   #op_defaults = CrowbarPacemakerHelper.op_defaults(node)
