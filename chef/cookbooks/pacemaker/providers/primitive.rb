72fd4488e59ab8d92db8b0e26b97af2e6c72674b
Abel Navarro <abel.navarro@gmail.com>
2017-11-24 10:28:35 +0100

    inotify

--- chef/cookbooks/pacemaker/providers/primitive.rb
+++ chef/cookbooks/pacemaker/providers/primitive.rb
@@ -77,8 +77,12 @@ end
 def update_resource(name)
   ops = new_resource.op
   Chef::Log.warn("XXX ops.inspect: #{ops.inspect}")
-  Chef::Log.warn("XXX current_default: #{new_resource.op.current_default}")
-  Chef::Log.warn("XXX current_default.class: #{new_resource.op.current_default.class}")
+  if new_resource.op.respond_to?(current_default)
+    Chef::Log.warn("XXX current_default: #{new_resource.op.current_default}")
+    Chef::Log.warn("XXX current_default.class: #{new_resource.op.current_default.class}")
+  else
+    Chef::Log.warn("XXX no current default")
+  end
   #Chef::Log.warn("XXX node[:pacemaker][:config][:op_defaults]: #{node[:pacemaker][:config][:op_defaults]}")
   #op_defaults = CrowbarPacemakerHelper.op_defaults(node)
 
